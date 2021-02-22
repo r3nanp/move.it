1485fe0e02e1eea4956c0e05ea3839cf2e5e35ab
r3nanp <renanmol87@gmail.com>
2021-02-22 11:32:40 -0300

    feat: create seo component

---
+++ src/components/SEO/SEO.tsx
@@ -0,0 +1,44 @@
+import Head from 'next/head'
+
+interface SEOProps {
+  title: string
+  shouldIndexPage?: boolean
+}
+
+function SEO({ title }: SEOProps) {
+  return (
+    <Head>
+      <title>{title}</title>
+
+      <link rel="shortcut icon" href="/favicon.png" />
+      <meta name="keywords" content="r3nanp, exercise" />
+      <meta
+        name="description"
+        content="A delivery app to your favorite products"
+      />
+
+      <meta property="og:site_name" content="Move It" />
+      <meta property="og:title" content="Move It" />
+      <meta
+        property="og:description"
+        content="A app to make you move."
+      />
+      <meta property="og:image" content="/move.it-thumb.svg" />
+      <meta name="twiiter:image:alt" content="Thumbnail" />
+      <meta property="og:image:type" content="image/svg" />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+
+      <meta name="twitter:image" content="/move.it-thumb.svg" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content="Move It" />
+      <meta
+        name="twitter:description"
+        content="A app to make you move."
+      />
+      <meta name="twiiter:create" content="r3nanp" />
+    </Head>
+  )
+}
+
+export { SEO }
