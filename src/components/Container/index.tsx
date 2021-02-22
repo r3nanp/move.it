86a19a87673e0a4eb92fbcc7cbf388c351fbc487
r3nanp <renanmol87@gmail.com>
2021-02-22 11:35:53 -0300

    feat: create container component

---
+++ src/components/Container/index.tsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Container = styled.main`
+  height: 100vh;
+  max-width: 992px;
+  margin: 0 auto;
+  padding: 2.5rem 2rem;
+
+  display: flex;
+  flex-direction: column;
+`
