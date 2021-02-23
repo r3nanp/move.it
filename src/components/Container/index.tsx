d4f652bbdd9c55c9511e0d2e49219f95455dca78
r3nanp <renanmol87@gmail.com>
2021-02-23 16:05:18 -0300

    style(container): add section styles

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -8,4 +8,13 @@ export const Container = styled.main`
 
   display: flex;
   flex-direction: column;
+
+  > section {
+    flex: 1;
+
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    gap: 6.25rem;
+    align-content: center;
+  }
 `
