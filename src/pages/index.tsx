e7eeb4af2d92e5fbff746a07d26427097f259f5e
r3nanp <renanmol87@gmail.com>
2021-02-23 16:08:57 -0300

    feat(index): add login oauth and handleSignIn function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,24 @@
+import { FormEvent, useCallback } from 'react'
+import { signIn } from 'next-auth/client'
+import { useRouter } from 'next/router'
 import { Container } from '../components/Container'
-import { ExperienceBar } from '../components/ExperienceBar'
 import { SEO } from '../components/SEO/SEO'
 
 export default function Home() {
+  const router = useRouter()
+
+  const handleSignIn = useCallback((event: FormEvent) => {
+    event.preventDefault()
+    signIn()
+
+    router.push('/exercise')
+  }, [])
+
   return (
     <Container>
-      <SEO title="Move.it" />
+      <SEO title="Move.it | Login" />
 
-      <ExperienceBar />
+      <button onClick={handleSignIn}>Faça login</button>
     </Container>
   )
 }
