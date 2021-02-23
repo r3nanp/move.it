790dd3382168aa32ebcd4efd727b6c1a473a2952
r3nanp <renanmol87@gmail.com>
2021-02-23 16:03:32 -0300

    feat(completed challenges): create component

---
+++ src/components/CompletedChallenges/index.tsx
@@ -0,0 +1,10 @@
+import { Container } from './styles'
+
+export function CompletedChallenges() {
+  return (
+    <Container>
+      <span>Desafios completos</span>
+      <span>2</span>
+    </Container>
+  )
+}
