716d0aa4425cbb523b0a8d480bc1c6178a41232d
r3nanp <renanmol87@gmail.com>
2021-02-24 17:46:26 -0300

    styles(container): change styles

--- src/components/Container/index.tsx
+++ src/components/Container/index.tsx
@@ -16,5 +16,47 @@ export const Container = styled.main`
     grid-template-columns: 1fr 1fr;
     gap: 6.25rem;
     align-content: center;
+
+    padding-top: 2rem;
+  }
+`
+
+export const LoginWrapper = styled.section`
+  background: var(--blue);
+  height: 100vh;
+
+  > div {
+    display: flex;
+    align-items: center;
+    height: 100%;
+
+    position: relative;
+
+    > div {
+      display: flex;
+      flex-direction: column;
+      text-align: center;
+
+      > h2 {
+        color: var(--white);
+        font-size: 2rem;
+      }
+
+      width: 25%;
+      position: absolute;
+      right: 60px;
+
+      button {
+        width: 100%;
+        height: 5rem;
+
+        margin-top: 2rem;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border: 0;
+        border-radius: 5px;
+      }
+    }
   }
 `
