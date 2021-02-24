2c216c2f310caa5d61117c7a28940f088e0976f9
r3nanp <renanmol87@gmail.com>
2021-02-24 17:56:26 -0300

    feat: add context provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,21 @@
 import { AppProps } from 'next/app'
 import { Provider } from 'next-auth/client'
 import { ThemeProvider } from 'styled-components'
+import { ChallengesProvider } from '../contexts/ChallengesContext'
 
 import { theme } from '../styles/theme'
 import GlobalStyles from '../styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Provider session={pageProps.session}>
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    </Provider>
+    <ChallengesProvider>
+      <Provider session={pageProps.session}>
+        <ThemeProvider theme={theme}>
+          <GlobalStyles />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </Provider>
+    </ChallengesProvider>
   )
 }
 
