05802dd2415141c2e4680fc887bf5a5e80d03201
r3nanp <renanmol87@gmail.com>
2021-02-26 19:43:21 -0300

    style(sidebar): add styles

--- src/components/Sidebar/styles.ts
+++ src/components/Sidebar/styles.ts
@@ -20,6 +20,9 @@ const iconsCss = css`
 
 export const Bar = styled.aside`
   position: fixed;
+  z-index: 1;
+  left: 0;
+
   height: 100%;
   width: 5rem;
   padding: 0.7rem;
@@ -33,6 +36,11 @@ export const Bar = styled.aside`
     display: flex;
     flex-direction: column;
     align-items: center;
+  }
+
+  @media screen and (max-width: 768px) {
+    width: 4rem;
+
   }
 `
 
