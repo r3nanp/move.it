65b4ccf27ffe44b4d65e5ab4a20e8a3141762087
r3nanp <renanmol87@gmail.com>
2021-02-26 19:44:43 -0300

    feat(document): add apple icon  and webmanifest

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -47,6 +47,13 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap"
             rel="stylesheet"
           />
+          <link rel="manifest" href="manifest.json" />
+
+          <link
+            rel="apple-touch-icon"
+            sizes="192x192"
+            href="/favicon@3x.png"
+          />
         </Head>
         <body>
           <Main />
