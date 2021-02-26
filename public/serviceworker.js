6660fe9ce7c8b7f2a1ba1e4077cbad2f28402c1c
r3nanp <renanmol87@gmail.com>
2021-02-26 19:19:39 -0300

    feat(service worker): create service worker

---
+++ public/serviceworker.js
@@ -0,0 +1,37 @@
+const cacheName = 'moveit'
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(async cache => {
+      await cache.addAll(['/', '/exercise'])
+      return self.skipWaiting()
+    })
+  )
+})
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches
+      .open(cacheName)
+      .then(cache => cache.match(event.request, { ignoreSearch: true }))
+      .then(response => {
+        return response || fetch(event.request)
+      })
+  )
+})
+
+self.addEventListener('activate', function activator(event) {
+  event.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(
+        keys
+          .filter(function (key) {
+            return key.indexOf(cacheName) !== 0
+          })
+          .map(function (key) {
+            return caches.delete(key)
+          })
+      )
+    })
+  )
+})
