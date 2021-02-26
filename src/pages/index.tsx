b7b2a141b2cd859e7941807d7cafabf7150d7e87
r3nanp <renanmol87@gmail.com>
2021-02-26 10:22:12 -0300

    feat(index): add images

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,19 +12,29 @@ export default function Home() {
     <LoginWrapper>
       <SEO title="Move.it | Login" />
       <div className="image-container">
-        <Image src="/" alt="Banner" width={300} height={300} />
+        <Image src="/logo-gradient.png" alt="Banner" width={760} height={660} />
       </div>
 
       <div className="left-container">
         <header>
-          <Image src="/logo-full.svg" alt="Logo" width={300} height={300} className="image" />
+          <Image
+            src="/logo-full.svg"
+            alt="Logo"
+            width={300}
+            height={300}
+            className="image"
+          />
         </header>
 
         <h2>Bem vindo</h2>
+        <p>
+          <GithubIcon />
+          Faça login com seu Github para começar
+        </p>
 
-        <button onClick={signIn}>
-          <GithubIcon /> Faça login
-        </button>
+        <div>
+          <button onClick={signIn}>Faça login</button>
+        </div>
       </div>
     </LoginWrapper>
   )
