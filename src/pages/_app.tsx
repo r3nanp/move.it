112eda5be9ebc55041e45d3b24af1c717c17e6b8
r3nanp <renanmol87@gmail.com>
2021-02-26 19:44:09 -0300

    feat(app): add service worker when component is mounted

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import { AuthProvider } from '../contexts/AuthContext'
@@ -6,6 +7,19 @@ import { theme } from '../styles/theme'
 import GlobalStyles from '../styles/global'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if ('serviceWorker' in navigator) {
+      navigator.serviceWorker
+        .register('/serviceworker.js')
+        .then(() => {
+          console.log('service worker registered')
+        })
+        .catch(() => {
+          console.warn('service worker failed')
+        })
+    }
+  }, [])
+
   return (
     <AuthProvider pageProps={pageProps.session}>
       <ThemeProvider theme={theme}>
