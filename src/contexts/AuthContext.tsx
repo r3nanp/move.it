bf0cd62a93ba07d3dcec118634aef03267d0fad0
r3nanp <renanmol87@gmail.com>
2021-03-17 19:50:34 -0300

    refactor: add conditional sign in and sign out

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,4 +1,11 @@
-import { createContext, FormEvent, useCallback } from 'react'
+import {
+  createContext,
+  FormEvent,
+  ReactElement,
+  ReactNode,
+  useCallback
+} from 'react'
+
 import {
   Provider,
   signOut as handleLogout,
@@ -11,7 +18,7 @@ export interface AuthContextData {
 }
 
 interface AuthProviderProps {
-  children: React.ReactNode
+  children: ReactNode
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   pageProps: any
 }
@@ -21,15 +28,25 @@ export const AuthContext = createContext({} as AuthContextData)
 export function AuthProvider({
   pageProps,
   children
-}: AuthProviderProps): JSX.Element {
+}: AuthProviderProps): ReactElement {
   const signIn = useCallback((event: FormEvent) => {
     event.preventDefault()
-    handleLogin('github', { callbackUrl: process.env.DEV_URL })
+    handleLogin('github', {
+      callbackUrl:
+        process.env.NODE_ENV !== 'production'
+          ? process.env.DEV_URL
+          : process.env.PRODUCTION_URL
+    })
   }, [])
 
   const signOut = useCallback((event: FormEvent) => {
     event.preventDefault()
-    handleLogout({ callbackUrl: process.env.NEXTAUTH_URL })
+    handleLogout({
+      callbackUrl:
+        process.env.NODE_ENV !== 'production'
+          ? process.env.DEV_LOGOUT_URL
+          : process.env.PRODUCTION_LOGOUT_URL
+    })
   }, [])
 
   return (
