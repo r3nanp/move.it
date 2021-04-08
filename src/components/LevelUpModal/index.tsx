baf40500c388ceb6c4f7a380f9b01b8b888ba4c4
r3nanp <renanmol87@gmail.com>
2021-04-08 14:42:26 -0300

    refactor: add aria hidden attribute

--- src/components/LevelUpModal/index.tsx
+++ src/components/LevelUpModal/index.tsx
@@ -1,18 +1,23 @@
+import { ReactElement } from 'react'
 import { useChallenges } from '../../hooks/useChallenges'
 import { Container, Overlay } from './styles'
 
-export function LevelUpModal(): JSX.Element {
-  const { level, closeLevelUpModal } = useChallenges()
+export function LevelUpModal(): ReactElement {
+  const { level, closeLevelUpModal, isLevelUpModalOpen } = useChallenges()
 
   return (
-    <Overlay>
+    <Overlay onClick={closeLevelUpModal} aria-hidden={!isLevelUpModalOpen}>
       <Container>
         <header>{level}</header>
 
         <strong>Parabéns</strong>
         <p>Você alcançou um novo level!</p>
 
-        <button type="button" onClick={closeLevelUpModal}>
+        <button
+          type="button"
+          onClick={closeLevelUpModal}
+          aria-hidden={!isLevelUpModalOpen}
+        >
           <img src="/icons/close.svg" alt="Fechar modal" />
         </button>
       </Container>
