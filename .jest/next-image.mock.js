5fbc32bdd082f0f8627831970d0d6c1a878cc5bf
Renan Pereira <renanmol87@gmail.com>
2021-07-23 20:25:20 -0300

    feat: create next-image mock

---
+++ .jest/next-image.mock.js
@@ -0,0 +1,9 @@
+import * as nextImage from 'next/image'
+
+Object.defineProperty(nextImage, 'default', {
+  configurable: true,
+  value: props => {
+    const { objectFit, ...rest } = props
+    return <img {...rest} />
+  }
+})
