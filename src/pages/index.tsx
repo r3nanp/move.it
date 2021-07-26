65f914a574031a0c4b8275c238c0fd461d9c0ce5
Renan Pereira <renanmol87@gmail.com>
2021-07-26 10:47:42 -0300

    refactor(home): add handle login

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { useAuth } from 'hooks/useAuth'
 import { SignInTemplate } from 'templates/sign-in'
 
 export default function Home(): JSX.Element {
-  const { signIn } = useAuth()
+  const { handleLogin } = useAuth()
 
-  return <SignInTemplate signIn={signIn} />
+  return <SignInTemplate handleLogin={handleLogin} />
 }
