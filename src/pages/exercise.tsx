ec890331ace945be5fc8566ab428425810cf6441
Renan Pereira <renanmol87@gmail.com>
2022-01-13 19:42:57 -0300

    feat(exercise): fetch users info from database

--- src/pages/exercise.tsx
+++ src/pages/exercise.tsx
@@ -1,10 +1,13 @@
 import { GetServerSideProps } from 'next'
+import { User } from 'types/User'
 
 import { ExerciseTemplate } from 'templates/exercise'
 import { protectedRoutes } from 'utils/protected-routes'
 import { getCookie } from 'utils/cookies'
+import { prisma } from 'utils/prisma'
 
 type ExerciseProps = {
+  user: User
   level: number
   currentExperience: number
   challengesCompleted: number
@@ -17,16 +20,30 @@ export default function ExercisePage(props: ExerciseProps): JSX.Element {
 export const getServerSideProps: GetServerSideProps = async context => {
   const session = await protectedRoutes(context)
 
-  const level = getCookie('level', context)
-  const currentExperience = getCookie('currentExperience', context)
-  const challengesCompleted = getCookie('challengesCompleted', context)
-
   if (!session) {
     return { props: {} }
   }
 
+  const level = getCookie('level', context)
+  const currentExperience = getCookie('currentExperience', context)
+  const challengesCompleted = getCookie('challengesCompleted', context)
+
+  const userData = await prisma.user.findUnique({
+    where: {
+      email: session.user.email
+    }
+  })
+
+  const user = {
+    id: userData.id,
+    name: userData.name,
+    email: userData.email,
+    avatar: userData.image
+  }
+
   return {
     props: {
+      user,
       level: Number(level),
       currentExperience: Number(currentExperience),
       challengesCompleted: Number(challengesCompleted)
