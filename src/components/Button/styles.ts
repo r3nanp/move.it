9ef2fa70bc467adb882148a2fea59060ea96dbe6
Renan Pereira <renan.souza.dev@gmail.com>
2022-01-20 20:13:11 -0300

    feat(components): add button variants

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,40 +1,50 @@
 import styled, { css } from 'styled-components'
-import { ButtonProps } from '.'
+import { Variant, Variants } from '.'
 
-type VariantModifier = Pick<ButtonProps, 'variant'>
-
-const getVariant = ({ variant }: VariantModifier) => {
-  const variants = {
-    green: '#4cd62b',
-    completed: 'var(--completed-button)',
-    failed: 'var(--failed-button)',
-    finished: 'var(--white)',
-    default: 'var(--green)'
-  }
-
-  return variants[variant] ?? variants.default
+type Modifier = {
+  variant?: Variant
 }
 
-export const Container = styled.button<VariantModifier>`
+const getBackground = (variant: Variant) => {
+  const variants: Variants = {
+    green: '#4cd62b',
+    'opaque-green': 'var(--completed-button)',
+    'opaque-red': 'var(--failed-button)',
+    red: 'var(--red)'
+  }
+
+  return variants[variant] ?? variants.green
+}
+
+const getColor = (variant: Variant) => {
+  const variants: Variants = {
+    green: 'var(--white)',
+    red: 'var(--white)',
+    'opaque-green': 'var(--green)',
+    'opaque-red': 'var(--red)'
+  }
+
+  return variants[variant] ?? variants.green
+}
+
+export const Container = styled.button<Modifier>`
   ${({ variant }) => css`
     width: 100%;
-    background: ${() => getVariant({ variant })};
-    color: #fff;
-    font-size: 1.5rem;
+    background: ${getBackground(variant)};
+    color: ${getColor(variant)};
+    font-size: 1rem;
     height: 4rem;
     display: flex;
     align-items: center;
+    font-weight: 600;
     justify-content: center;
     border: 0;
     border-radius: 5px;
-    transition: filter 0.2s;
+    transition: all 0.2s ease-in-out;
     &:disabled {
       background: var(--white);
       color: var(--text);
       cursor: not-allowed;
-    }
-    &:hover {
-      filter: brightness(0.9);
     }
   `}
 `
