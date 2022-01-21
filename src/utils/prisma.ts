8b96c63439401ce345804d9503fb50d7e825f4c9
Renan Pereira <renan.souza.dev@gmail.com>
2022-01-21 15:13:23 -0300

    chore(deps): bump nextjs and add new deps

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,5 +1,11 @@
 import { PrismaClient } from '@prisma/client'
 
+// PrismaClient is attached to the `global` object in development to prevent
+// exhausting your database connection limit.
+//
+// Learn more:
+// https://pris.ly/d/help/next-js-best-practices
+
 let prisma: PrismaClient
 
 if (process.env.NODE_ENV === 'production') {
