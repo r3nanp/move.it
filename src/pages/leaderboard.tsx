426cd841c1914885827c953416a9cacf9d6440d9
Renan Pereira <renan.souza.dev@gmail.com>
2022-09-18 14:13:20 -0300

    feat(leaderboard): create leaderboard and fetch content

--- src/pages/leaderboard.tsx
+++ src/pages/leaderboard.tsx
@@ -1,10 +1,12 @@
 import { GetServerSideProps } from 'next'
+import { prisma } from 'utils/prisma'
 import { protectedRoutes } from 'utils/protected-routes'
 
 import { LeaderboardTemplate } from 'templates/leaderboard'
+import { User } from '@prisma/client'
 
-export default function Leaderboard(): JSX.Element {
-  return <LeaderboardTemplate />
+export default function Leaderboard({ users }: { users: User[] }): JSX.Element {
+  return <LeaderboardTemplate users={users} />
 }
 
 export const getServerSideProps: GetServerSideProps = async context => {
@@ -14,7 +16,19 @@ export const getServerSideProps: GetServerSideProps = async context => {
     return { props: {} }
   }
 
+  const users = await prisma.user.findMany({
+    orderBy: { totalExperience: 'desc' }
+  })
+
+  const usersFormatted = users.map(user => ({
+    ...user,
+    createdAt: JSON.stringify(user.createdAt),
+    updatedAt: JSON.stringify(user.updatedAt)
+  }))
+
   return {
-    props: {}
+    props: {
+      users: usersFormatted
+    }
   }
 }
